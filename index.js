12e84fb62eb805f6db9ca795b99ecf5fe723b330
victor <victor.njutha@student.moringaschool.com>
2023-10-03 14:42:39 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//saturdayFun
+function saturdayFun (activity = "roller-skate") {
+    return "This saturday, I want to ${activity}!";
+}
+
+
+//mondayWork
+function mondayWork (task= "go to the office") {
+    return "This Monday, I will ${task}.";
+}
+
+
+//wrapAdjective
+function wrapAdjective(adjective="special",prompt= "*") {
+    return function (param= "good") {
+        return  "You are ${prompt}${adjective}${prompt}${param}${prompt}${adjective}${prompt}";
+    };
+}
